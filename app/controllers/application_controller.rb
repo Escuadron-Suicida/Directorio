d0aabdfc9ecaf2856a82fd03b03ff376d9ab7a88
juiko <pulga.p.29@gmail.com>
2017-05-21 14:17:51 -0700

    editar empresa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,17 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,
-                                                       :rut,
-                                                       :legal_name,
-                                                       :agent_name,
-                                                       :agent_rut,
-                                                       :phone,
-                                                       :contact_name,
-                                                       :contact_phone,
-                                                       :address])
+    parameters = [:name,
+                  :rut,
+                  :legal_name,
+                  :agent_name,
+                  :agent_rut,
+                  :phone,
+                  :contact_name,
+                  :contact_phone,
+                  :address]
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: parameters)
+    devise_parameter_sanitizer.permit(:account_update, keys: parameters)
   end
 end
