2f9f0e583e1027b5466502bccf0fc60422318d99
juiko <pulga.p.29@gmail.com>
2017-06-03 17:49:56 -0700

    tamaño para mostrar las imagenes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :business
 
-  has_attached_file :avatar, styles: {medium:"300x300",thum:"100x100"},default_url: "missing.png"
+  has_attached_file :avatar, styles: {medium:"200x200",thum:"100x100"},default_url: "missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   def self.search(search)
